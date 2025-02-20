d95a4287b99a083ac69c54fdd2c1123f0c712793
Rob Hicks <robert.hicks@familysearch.org>
2025-02-20 16:48:32 -0700

    Update Svelte dependency to 5.20.2 and add reactive store implementation

--- packages/svelte-5/src/index.ts
+++ packages/svelte-5/src/index.ts
@@ -4,7 +4,7 @@ import { Observer, reactive } from "@reactivedata/reactive";
  * Svelte helper to enable smart Reactive rerendering of your component.
  *
  * The usage of the return value is automatically tracked.
- * The component where you use this value is then rerendered automatically if any of the
+ * The component where you use this store is then rerendered automatically if any of the
  * used values change.
  *
  * e.g.:
@@ -20,20 +20,13 @@ import { Observer, reactive } from "@reactivedata/reactive";
  *
  * Now, your component only rerenders when Bob's name changes
  *  (or if the second element of the array changes)
- *
+ * 
  * Binding values works also.
  * <input type="text" bind:value={store.items[1].name} />
  */
 export function svelteSyncedStore<T>(syncedObject: T) {
-  let value = $state(
-    reactive(
-      syncedObject,
-      new Observer(() => {
-        // Force a reactivity update when the underlying data changes
-        value = { ...value };
-      })
-    )
-  );
+  const observer = new Observer(() => {});
+  const store = reactive(syncedObject, observer);
 
-  return value;
+  return store;
 }
